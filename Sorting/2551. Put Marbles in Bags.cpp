40de2b39d24b8f05fcae478e98cf5ee6d786f10c
Pawan Giri <49859885+123pk@users.noreply.github.com>
2023-03-11 22:26:04 +0530

    Create 2551. Put Marbles in Bags.cpp

---
+++ Sorting/2551. Put Marbles in Bags.cpp
@@ -0,0 +1,6 @@
+/*
+Platform :- Leetcode
+Problem link :- https://leetcode.com/problems/put-marbles-in-bags/description/
+Video Explanation (Hindi) :- https://youtu.be/xJfTvpGbDXo
+Code :- https://leetcode.com/problems/put-marbles-in-bags/submissions/913304965/
+*/
