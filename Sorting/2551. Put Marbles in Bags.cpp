16c6506a8098c5fe53b49e6b3a2e4dd6d86333de
Pawan Giri <49859885+123pk@users.noreply.github.com>
2023-03-11 22:41:49 +0530

    Update 2551. Put Marbles in Bags.cpp

--- Sorting/2551. Put Marbles in Bags.cpp
+++ Sorting/2551. Put Marbles in Bags.cpp
@@ -4,3 +4,36 @@ Problem link :- https://leetcode.com/problems/put-marbles-in-bags/description/
 Video Explanation (Hindi) :- https://youtu.be/xJfTvpGbDXo
 Code :- https://leetcode.com/problems/put-marbles-in-bags/submissions/913304965/
 */
+ 
+class Solution {
+public:
+    
+    long long putMarbles(vector<int>& weight, int k) {
+        long long ans = 0;
+        
+        long long tot1 = 0 , tot2 = 0;
+        int n = weight.size();
+        // effect of one partition is 
+        //sum of weights[i] + weights[i+1] 
+        //if we partition between i and i+1
+        vector<long long>values;
+        for(int i=1;i<n;++i){
+            //finding all possible sum for adjacent pairs 
+            long long z = weight[i] + weight[i-1];
+            values.push_back(z);
+        }
+
+        //sorting them in increasing order
+        sort(values.begin(),values.end());
+        int m = values.size();
+
+        //first k-1 values will give us the minimum score
+        //last k-1 values will give us the maximum score
+        for(int i=0;i<(k-1);++i){
+           tot1 += values[i];
+           tot2 += values[m-1-i];
+        }
+    
+        return tot2 - tot1;
+    }
+};
