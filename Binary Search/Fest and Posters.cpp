0d4647d0156da3c1ae951b4402e38eeee4829724
Pawan Giri <49859885+123pk@users.noreply.github.com>
2023-03-22 08:30:22 +0530

    Create Fest and Posters.cpp

---
+++ Binary Search/Fest and Posters.cpp
@@ -0,0 +1,9 @@
+/*
+Platform :- GeeksforGeeks
+Link :- https://practice.geeksforgeeks.org/contest/job-a-thon-18-hiring-challenge/problems#
+Approach :- Binary Search in range (1 to 10^15) ,
+            why till 10^15 , because we can finish all the given problems in worst case in (10^7)*10^7 time = 10^14 , here in worst case you can consider 
+            there is just one student and he take 10^7 time per problem and you have to solve 10^7 problems.
+            
+Time Complexity :- O( n log n)
+*/
