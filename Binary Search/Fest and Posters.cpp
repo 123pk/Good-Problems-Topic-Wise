23d0619bbec980e58c650a7d9b7503fae5352b66
Pawan Giri <49859885+123pk@users.noreply.github.com>
2023-03-22 08:30:55 +0530

    Update Fest and Posters.cpp

--- Binary Search/Fest and Posters.cpp
+++ Binary Search/Fest and Posters.cpp
@@ -7,3 +7,93 @@ Approach :- Binary Search in range (1 to 10^15) ,
             
 Time Complexity :- O( n log n)
 */
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+ // } Driver Code Ends
+class Solution {
+  public:
+    long long minimumTime(int N, int NUM, vector<int> &time) {
+        // code here
+        //binary search
+        
+        long long start = 1,end = 1e15,mid = (start+end)/2ll;
+        long long ans = 1e15;
+        while(start<=end){
+            
+            long long score = 0;
+            for(auto&x:time){
+                long long val = x;
+                val = mid/x;
+                score += val;
+            }
+            
+            if(score >= NUM){
+                //reduce the time
+                ans = min(ans,mid);
+                end = mid-1ll;
+                mid = (start+end)/2ll;
+            }
+            else{
+                start = mid+1ll;
+                mid  = (start+end)/2ll;
+            }
+        }
+        
+        return ans;
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N;
+        scanf("%d",&N);
+        
+        
+        int NUM;
+        scanf("%d",&NUM);
+        
+        
+        vector<int> time(N);
+        Array::input(time,N);
+        
+        Solution obj;
+        long long res = obj.minimumTime(N, NUM, time);
+        
+        cout<<res<<endl;
+        
+    }
+}
+  // } Driver Code Ends
